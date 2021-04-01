2a744365e3caf1cbcd7f0857e28563f3b39a0a36
Riverr-OM <1460115420@qq.com>
2021-04-01 11:46:00 +0800

    example4

---
+++ script.js
@@ -0,0 +1,59 @@
+document.write('Hello,world!');
+
+function sayHello(){
+  var response = prompt("what's ur name?");
+  alert('Hello' + response + 'have a good day!');
+}
+
+function add(a,b){
+  return a+b;
+}
+console.log(add(20,30));
+console.log(add(100,30));
+
+function ShowTopic(){
+  var x = document.getElementById('demo');
+  x.style.fontSizen = '25PX';
+  x.style.color = 'red';
+}
+
+var movies = ['Avengers','Captain Marvel','Black Panther','Spiderman'];
+movies[0]='Wonder Woman';
+movies.push('Avengers');
+movies.pop();
+console.log(movies);
+
+var movie = {
+  title:'CaptSain Marvel',
+  time:"2pm"
+}
+alert(movie.title);
+movie.status='available';
+console.log(movie);
+
+var fruits = [];
+for(i=1;i<10;i++){
+
+}
+
+var movieDetails = [
+{title: "Avengers",
+time:"12pm",
+   status: "available"},
+{title: "Captain Marvel",
+       time:"2pm",
+         status: "unavailable"},
+{title: "Black Panther",
+            time:"4pm",
+              status: "available"},
+{title: "Spiderman",
+                 time:"6pm",
+                    status: "available"} ];
+
+for (var movie of movieDetails){
+  if (movie.status === "available"){
+     console.log("The movie " + movie.title + " plays at " + movie.time)
+ }
+else{  console.log("Sorry, the movie " + movie.title + " is sold out.")
+ }
+};
